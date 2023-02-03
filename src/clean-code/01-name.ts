f77b91e4f5116222bce764142d78ee502003e51f
Olinto <olintojose@gmail.com>
2023-02-03 13:22:54 -0300

    names by tipes

--- src/clean-code/01-name.ts
+++ src/clean-code/01-name.ts
@@ -9,10 +9,10 @@
         { id: 4, flagged: false },
         { id: 5, flagged: false },
         { id: 7, flagged: true },
-    ]; 
-    
+    ];
+
     // Archivos marcados para borrar - files to delete
-    const filesToDelete = filesToEvaluate.map( file => file.flagged );
+    const filesToDelete = filesToEvaluate.map(file => file.flagged);
 
     //MAlos nombres
     class AbstractUser { };
@@ -26,41 +26,75 @@
 
 
     // Todo: Tarea
-        
+
     // día de hoy - today
     //const ddmmyyyy = new Date();
     const today = new Date();
-    
+
     // días transcurridos - elapsed time in days
-//    const d: number = 23;
+    //    const d: number = 23;
     const elapsedTimeInDays: number = 23;
 
-    
+
     // número de archivos en un directorio - number of files in directory
     //const dir = 33;
     const numberOfFilesInDirectory = 33;
-    
+
     // primer nombre - first name
     //const nombre = 'Fernando';
     const firstName = 'Fernando';
 
-    
+
     // primer apellido - last name
-   // const apellido = 'Herrera';
+    // const apellido = 'Herrera';
     const lastName = 'Herrera';
 
 
     // días desde la última modificación - days since modification
     //const dsm = 12;
     const daysSinceLastModification = 12;
-    
+
     // cantidad máxima de clases por estudiante - max classes per student
     //const max = 6;
     const maxClassesPerStudent = 6;
+
+    //Boolenos
+    const isOpen = true;
+    const canWrite = true;
+    const hasFruit = true;
+    const isActive = false;
+    const hasValues = false;
+    const isEmpty = false;
+    //numeros
+
+
+    const maxFruits = ;
+    const minFruits = 1;
+    const totalFruits = 3;
+
+    const totalOfCars = 4;
+
+    //Funciones
+
+    createUser();
+    updateUser();
+    sendEmail();
 
 
 
 })();
 
 
+
+function createUser() {
+    throw new Error("Function not implemented.");
+}
+
+function updateUser() {
+    throw new Error("Function not implemented.");
+}
+
+function sendEmail() {
+    throw new Error("Function not implemented.");
+}
 
